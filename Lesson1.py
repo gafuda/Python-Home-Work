a34e07e8aa8e620a6918a500a3cf7c6094ea3061
Mariia <marii.ch@bk.ru>
2022-12-16 01:20:42 +0300

    first commit

---
+++ Lesson1.py
@@ -0,0 +1,22 @@
+# a = int(input ('Введите а: '))
+# b = int(input ('Введите b: '))
+
+# if (a == b**2 or b == a**2):
+#     print ("Квадрат")
+# else:
+#     print('нет')
+
+
+# a = int(input ('Введите а: '))
+# b = int(input ('Введите b: '))
+# c = int(input ('Введите c: '))
+# max
+# if a > b and a > c:
+#     max = a
+# if b > a and b > c:
+#     max = b
+# if c > a and c > b:
+#     max = c
+
+# print (c)
+
